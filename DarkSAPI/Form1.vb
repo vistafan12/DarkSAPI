6a337b9fcd8ec089a28cd4e4c4da6c97b648a54f
VistaFan12 <kroljestboss@o2.pl>
2017-02-11 12:28:40 +0100

    added info

--- DarkSAPI/Form1.vb
+++ DarkSAPI/Form1.vb
@@ -16,10 +16,8 @@
     Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
         End
     End Sub
-    /// trzeba dodać coś innego niż "MsgBox" :P ///
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
-        MsgBox("DarkSAPI (C) 2017 kuba0303, All Right Reserved.
-GNU GPL License, Made in Visual Studio Ultimate 2010.
-Website: kuba0303.ga")
+        Form2.Show()
     End Sub
 End Class
+
